c7fe22b12963b6f4da8a69c4d2dbcc5d90373260
Md. Raisul Islam <raisulislam.cse3.bu@gmail.com>
2023-04-12 15:38:46 +0600

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import Tags from "../components/tags/Tags"
+import VideoGrid from "../components/grid/VideoGrid"
+import Pagination from "../components/ui/Pagination"
+
+export default function Home () {
+	return (
+		<>
+		<Tags />
+		<VideoGrid />
+		<Pagination />
+		</>
+	)
+}
